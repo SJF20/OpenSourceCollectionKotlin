26ba065e2ca1cef085fbb805714617a2ee50835c
ShiJingFeng <1634886078@qq.com>
2020-08-28 18:04:19 +0800

    同步

--- library_module/base/src/main/java/com/shijingfeng/base/base/adapter/BaseFragmentPagerAdapter.kt
+++ library_module/base/src/main/java/com/shijingfeng/base/base/adapter/BaseFragmentPagerAdapter.kt
@@ -2,6 +2,7 @@ package com.shijingfeng.base.base.adapter
 
 import android.util.SparseArray
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.shijingfeng.base.base.fragment.BaseFragment
